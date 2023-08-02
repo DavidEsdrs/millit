62ed756b9e93f01c8a4f666a0243303fbf5e1376
DavidEsdrs <carpina1983@gmail.com>
2023-08-02 06:39:50 -0300

    0.0.7: types declaration corrected

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,20 @@
 export = millit;
 /**
- * Parse val to milliseconds
+ * Parse milliseconds to string representation
  *
  *
- * @param {String|Number} time
+ * @param {Number} time
  * @throws {Error} throw an error if val is an empty string or is not a number
- * @return {String|Number}
+ * @return {String}
  */
-declare function millit(time: string | number): string | number;
+declare function millit(time: number): string;
+
+/**
+ * Parse time string to milliseconds
+ *
+ *
+ * @param {String} time
+ * @throws {Error} throw an error if val is an empty string or is not a number
+ * @return {Number}
+ */
+declare function millit(time: string): number;
